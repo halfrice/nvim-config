85eb04ab04bbc7183a1ffb7512cb02bd9716e133
halfrice <halfrice@protonmail.com>
2025-06-05 22:12:38 -0700

    Install treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,56 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  lazy = "false",
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+              return true
+          end
+        end,
+      },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+           init_selection = "gnn",
+           node_incremental = "grn",
+           scope_incremental = "grc",
+           node_decremental = "grm",
+        },
+      },
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "query",
+        "vimdoc",
+        "c",
+        "python",
+        "rust",
+        "astro",
+      },
+    })
+  end,
+}
+
